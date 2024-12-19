cd2d4f4c07cb4b23d1bac6efd86929d6c46e97da
Arturo Romero <116369556+RArtutos@users.noreply.github.com>
2024-12-19 00:53:11 -0600

    Delete backend directory

--- backend/app/db/repositories/__init__.py
+++
@@ -1,9 +0,0 @@
-from .user_repository import UserRepository
-from .account_repository import AccountRepository
-from .user_account_repository import UserAccountRepository
-
-__all__ = [
-    'UserRepository',
-    'AccountRepository',
-    'UserAccountRepository'
-]
